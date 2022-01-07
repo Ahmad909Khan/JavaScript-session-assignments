58a2db78a5265963d168379ad53a2d6659fc1ee0
Ahmad909Khan <you@example.com>
2022-01-07 12:30:28 +0530

    Change condition value from "20<" to "20<="

--- grocery/js/grocery.js
+++ grocery/js/grocery.js
@@ -2,7 +2,7 @@ function calc(e) {
     if (money.value > 35) {
         var r = money.value - 35;
         alert('You can buy Grocery & Ice-cream with ' + r + 'Rs remaining');
-    } else if (20 < money.value && money.value < 35) {
+    } else if (20 <= money.value && money.value < 35) {
         var r = money.value - 20;
         alert('You can only buy Grocery, not Ice-cream. ' + r + 'Rs remaining');
     } else {
